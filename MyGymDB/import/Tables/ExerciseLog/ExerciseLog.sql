c0e44663d42328e93725658d285a53d8b64d4901
Richard Griffiths <griff182uk@yahoo.co.uk>
2016-09-20 19:43:40 +0100

    amended activity and sex tables

---
+++ MyGymDB/import/Tables/ExerciseLog/ExerciseLog.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [import].[ExerciseLog]
+(
+	[ExerciseLogID] INT NOT NULL CONSTRAINT PK_ExerciseLogID PRIMARY KEY, 
+    [ExerciseID] SMALLINT NOT NULL, 
+    [ActivityDate] DATETIME2 NOT NULL, 
+    [ExerciseCount] TINYINT NOT NULL, 
+    [GymUserId] INT NOT NULL, 
+    [FirstInserted] DATETIME2 NOT NULL DEFAULT getdate(), 
+    CONSTRAINT [FK_ExerciseLog_Exercise] FOREIGN KEY ([ExerciseId]) REFERENCES [import].[Exercise]([ExerciseId]), 
+    CONSTRAINT [FK_ExerciseLog_GymUser] FOREIGN KEY ([GymUserId]) REFERENCES [import].[GymUser]([GymUserId])
+)
